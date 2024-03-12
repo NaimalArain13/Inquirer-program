fb9c17d7314f3e44b2ca7a8c42609fc4c2a30c56
Naimal Salahuddin <136015168+NaimalArain13@users.noreply.github.com>
2024-03-13 02:03:38 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+import inquirer from "inquirer";
+//async function can be pause and resume the execution and it works through events
+async function main() {
+    //Inquirer.prompt tasks an array of question objects
+    const answers = await inquirer.prompt([
+        //this is the question object as an argument
+        {
+            name: "name",
+            type: "input",
+            message: "What is your name?" //this is question being dispplayed on console
+        }
+    ]);
+    //here i am using object desctruting syntax.
+    //it takes name property from object 'answers' and store in the same 'name' variable
+    const { name } = answers;
+    //it can be either done this way
+    //const name = answers.name
+    console.log(`Hello, ${name}! Welcome to our CLI application.`);
+}
+main();
